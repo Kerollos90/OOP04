fea751c575af4da379667e1825b9973f3e507edd
emadk <emadk@KEROLLOS>
2024-07-18 09:15:16 +0300

    Add project files.

---
+++ OOP04/IShape.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP04
+{
+    public interface IShape
+    {
+        public double Area { get;  }
+        void DisplayAreaInfo();
+    }
+
+    public interface ICircle : IShape
+    {
+       public  double Radius { get => Math.PI * Radius * Radius; }
+
+
+
+    }
+
+    public interface IRectangle : IShape
+    {
+        public double Height { get; set; }  
+        public double Width { get; set; }
+
+    }
+
+
+    public class Circle() : ICircle
+    {
+      
+
+        public double Area { get => Math.PI * Radius * Radius; }
+
+        public double Radius { get; set; }
+
+     
+
+        public void DisplayAreaInfo()
+        {
+            Console.WriteLine($" Circle:: Radius = {Radius} :: Area = { Area}");
+        }
+    }
+
+    public class  Rectangle () : IRectangle
+    {
+        public double Area { get => Width * Height; }
+        public double Height { get; set ; }
+        public double Width { get ; set ; }
+
+        public void DisplayAreaInfo()
+        {
+            Console.WriteLine($"  Rectangle :: Width = {Width} , Height = {Height} :: Area = {Area} "); 
+        }
+    }
+
+   
+            
+
+
+}
---
+++ OOP04/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace OOP04
+{
+
+
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            #region Part 2
+
+            #region Q1
+            //Circle circle = new Circle();
+            //Console.WriteLine("input radius");
+            //circle.Radius = double.Parse(Console.ReadLine());
+            //circle.DisplayAreaInfo();
+
+            //Console.WriteLine("========");
+
+            //Rectangle rectangle = new Rectangle();
+            //Console.WriteLine("input Width");
+            //rectangle.Width = double.Parse(Console.ReadLine());
+            //Console.WriteLine("input Height");
+            //rectangle.Height = double.Parse(Console.ReadLine());
+            //rectangle.DisplayAreaInfo(); 
+            #endregion
+
+            #region Q2
+
+
+
+
+            #endregion
+
+            #endregion
+
+
+
+        }
+    }
+}
